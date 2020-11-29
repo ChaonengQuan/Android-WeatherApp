8d8a0879f30a0b6678f886254891ac1c5daa4a89
Thomas Ruff <truff@email.arizona.edu>
2020-11-28 22:59:23 -0700

    Built initial UI and launches it upon starting the app

--- app/src/main/java/com/csc317/weatherapp/MainActivity.java
+++ app/src/main/java/com/csc317/weatherapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.csc317.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DisplayFragment weatherDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        weatherDisplay = new DisplayFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.weather_layout_container,weatherDisplay);
+        transaction.addToBackStack(null);
+        transaction.commit();
         setContentView(R.layout.activity_main);
     }
 }
